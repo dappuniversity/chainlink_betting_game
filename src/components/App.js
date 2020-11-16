0518002356278e5a826ef28f401d965e0be5caed
xternet <majewski.post@gmail.com>
2020-11-16 18:17:32 +0100

    .

--- src/components/App.js
+++ src/components/App.js
@@ -9,56 +9,53 @@ class App extends Component {
   
   async componentWillMount() {
     await this.loadWeb3()
-    await this.loadBlockchainData()
   }
 
-  async loadBlockchainData() {
-    const networkId = await this.state.web3.eth.net.getId()
-
-    if(networkId!==4){
-      this.setState({wrongNetwork: true})
-    } else {
-      const contract_abi = [{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "sender","type": "address"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"}],"name": "Received","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "uint256","name": "id","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "bet","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "randomSeed","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"},{"indexed": false,"internalType": "address","name": "player","type": "address"},{"indexed": false,"internalType": "uint256","name": "winAmount","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "randomResult","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "time","type": "uint256"}],"name": "Result","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "address","name": "admin","type": "address"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"}],"name": "Withdraw","type": "event"},{"inputs": [{"internalType": "uint256","name": "bet","type": "uint256"},{"internalType": "uint256","name": "seed","type": "uint256"}],"name": "game","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "bytes32","name": "requestId","type": "bytes32"},{"internalType": "uint256","name": "randomness","type": "uint256"}],"name": "rawFulfillRandomness","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "bytes32","name": "_keyHash","type": "bytes32"},{"internalType": "uint256","name": "_fee","type": "uint256"},{"internalType": "uint256","name": "_seed","type": "uint256"}],"name": "requestRandomness","outputs": [{"internalType": "bytes32","name": "requestId","type": "bytes32"}],"stateMutability": "nonpayable","type": "function"},{"stateMutability": "payable","type": "receive"},{"inputs": [{"internalType": "uint256","name": "random","type": "uint256"}],"name": "verdict","outputs": [],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "withdrawEther","outputs": [],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "withdrawLink","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "admin","outputs": [{"internalType": "address payable","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "ethInUsd","outputs": [{"internalType": "int256","name": "","type": "int256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "gameId","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "","type": "uint256"}],"name": "games","outputs": [{"internalType": "uint256","name": "id","type": "uint256"},{"internalType": "uint256","name": "bet","type": "uint256"},{"internalType": "uint256","name": "seed","type": "uint256"},{"internalType": "uint256","name": "amount","type": "uint256"},{"internalType": "address payable","name": "player","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "lastGameId","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "bytes32","name": "","type": "bytes32"}],"name": "nonces","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "randomResult","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "weiInUsd","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"}]
-      const contract_address = '0x2FeF79F6b8777D4C13E2D7be422628A5B458b7ad' //rinkeby
-      this.setState({contractAddress: contract_address})
-
-      const contract = new this.state.web3.eth.Contract(contract_abi, contract_address);
-      this.setState({ contract: contract })
-
-      const accounts = await this.state.web3.eth.getAccounts()
-      this.setState({ account: accounts[0] })
-
-      const balance = await this.state.web3.eth.getBalance(this.state.account)
-      this.setState({ balance: balance })
-
-      const maxBet = await this.state.web3.eth.getBalance(contract_address)
-      this.setState({ maxBet: maxBet })
-
-      const minBet = await contract.methods.weiInUsd().call()
-      this.setState({ minBet: minBet })
-    }
-  }
-
-
+  /** !UPDATE **/
   async loadWeb3() {
-    if(typeof window.ethereum!=='undefined'){
-      let network, balance, web3, maxBet, minBet
+    if(typeof window.ethereum!=='undefined' && !this.state.wrongNetwork){
+      let accounts, network, balance, web3, maxBet, minBet, contract, contract_abi, contract_address
 
+      //don't refresh DApp when user change the network
       window.ethereum.autoRefreshOnNetworkChange = false;
+
       web3 = new Web3(window.ethereum)
       this.setState({web3: web3})
 
-      //Update address&account when MM user change account
+      contract_abi = [{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "sender","type": "address"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"}],"name": "Received","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "uint256","name": "id","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "bet","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "randomSeed","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"},{"indexed": false,"internalType": "address","name": "player","type": "address"},{"indexed": false,"internalType": "uint256","name": "winAmount","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "randomResult","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "time","type": "uint256"}],"name": "Result","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "address","name": "admin","type": "address"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"}],"name": "Withdraw","type": "event"},{"inputs": [{"internalType": "uint256","name": "bet","type": "uint256"},{"internalType": "uint256","name": "seed","type": "uint256"}],"name": "game","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "bytes32","name": "requestId","type": "bytes32"},{"internalType": "uint256","name": "randomness","type": "uint256"}],"name": "rawFulfillRandomness","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "bytes32","name": "_keyHash","type": "bytes32"},{"internalType": "uint256","name": "_fee","type": "uint256"},{"internalType": "uint256","name": "_seed","type": "uint256"}],"name": "requestRandomness","outputs": [{"internalType": "bytes32","name": "requestId","type": "bytes32"}],"stateMutability": "nonpayable","type": "function"},{"stateMutability": "payable","type": "receive"},{"inputs": [{"internalType": "uint256","name": "random","type": "uint256"}],"name": "verdict","outputs": [],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "withdrawEther","outputs": [],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "withdrawLink","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "admin","outputs": [{"internalType": "address payable","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "ethInUsd","outputs": [{"internalType": "int256","name": "","type": "int256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "gameId","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "","type": "uint256"}],"name": "games","outputs": [{"internalType": "uint256","name": "id","type": "uint256"},{"internalType": "uint256","name": "bet","type": "uint256"},{"internalType": "uint256","name": "seed","type": "uint256"},{"internalType": "uint256","name": "amount","type": "uint256"},{"internalType": "address payable","name": "player","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "lastGameId","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "bytes32","name": "","type": "bytes32"}],"name": "nonces","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "randomResult","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "weiInUsd","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"}]
+      contract_address = '0x2FeF79F6b8777D4C13E2D7be422628A5B458b7ad' //rinkeby
+      contract = new web3.eth.Contract(contract_abi, contract_address);
+      accounts = await web3.eth.getAccounts()
+      
+      //Update the data when user initially connect
+      if(typeof accounts[0]!=='undefined' && accounts[0]!==null) {
+        balance = await web3.eth.getBalance(accounts[0])
+        maxBet = await web3.eth.getBalance(contract_address)
+        minBet = await contract.methods.weiInUsd().call()
+        this.setState({account: accounts[0], balance: balance, minBet: minBet, maxBet: maxBet})
+      }
+
+      this.setState({
+        contract: contract,
+        contractAddress: contract_address
+      })
+
+      //Update account&balance when user change the account
       window.ethereum.on('accountsChanged', async (accounts) => {
-        if(typeof accounts[0] !== 'undefined'){
+        if(typeof accounts[0] !== 'undefined'  && accounts[0]!==null){
           balance = await web3.eth.getBalance(accounts[0])
-          this.setState({account: accounts[0], balance: balance, loading: false })
+          maxBet = await web3.eth.getBalance(contract_address)
+          minBet = await contract.methods.weiInUsd().call()
+          
+          this.setState({account: accounts[0], balance: balance, minBet: minBet, maxBet: maxBet})
+        } else {
+          this.setState({account: null, balance: 0})
         }
       });
 
+      //Update data when user switch the network
       window.ethereum.on('chainChanged', async (chainId) => {
-        // this.setState({network: null, balance: null, loading: true, onlyNetwork: true})
-        network = await this.state.web3.eth.net.getId() 
+        network = parseInt(chainId, 16)
         if(network!==4){
           this.setState({wrongNetwork: true})
         } else {
@@ -67,7 +64,7 @@ class App extends Component {
             maxBet = await this.state.web3.eth.getBalance(this.state.contractAddress)
             minBet = await this.state.contract.methods.weiInUsd().call()
             
-            this.setState({balance: balance, maxBet: maxBet, minBet: minBet })
+            this.setState({ balance: balance, maxBet: maxBet, minBet: minBet })
           }
           this.setState({ network: network, loading: false, onlyNetwork: false, wrongNetwork: false})
         }
@@ -80,8 +77,10 @@ class App extends Component {
     const networkId = await this.state.web3.eth.net.getId() 
     if(networkId!==4) {
       this.setState({wrongNetwork: true})
-    } else {
+    } else if(typeof this.state.account !=='undefined' && this.state.account !==null){
       var randomSeed = Math.floor(Math.random() * Math.floor(1e9))
+
+      //Send bet to the contract and wait for the verdict
       this.state.contract.methods.game(bet, randomSeed).send({from: this.state.account, value: amount}).on('transactionHash', (hash) => {
         this.setState({ loading: true })
         this.state.contract.events.Result({}, async (error, event) => {
@@ -91,14 +90,20 @@ class App extends Component {
           } else {
             window.alert('WIN!')
           }
-          const balance = await this.state.web3.eth.getBalance(this.state.account)
-          const maxBet = await this.state.web3.eth.getBalance(this.state.contractAddress)
 
-          this.setState({ loading: false, balance: balance, maxBet: maxBet })
+          //Prevent error when user logout, while waiting for the verdict
+          if(this.state.account!==null && typeof this.state.account!=='undefined'){
+            const balance = await this.state.web3.eth.getBalance(this.state.account)
+            const maxBet = await this.state.web3.eth.getBalance(this.state.contractAddress)
+            this.setState({ balance: balance, maxBet: maxBet })
+          }
+          this.setState({ loading: false })
         })
       }).on('error', (error) => {
         window.alert('Error')
       })
+    } else {
+      window.alert('Problem with account or network')
     }
   }
 
@@ -129,7 +134,6 @@ class App extends Component {
   }
 
   render() {
-    console.log('network: ', this.state.network)
     return (
       <div>
         <Navbar account={this.state.account}/>&nbsp;
